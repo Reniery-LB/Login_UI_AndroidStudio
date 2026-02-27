2ef90e0abd1e0aa03e9ca5e836dd33fc42fb834a
Reniery-LB <relu_23@alu.uabcs.mx>
2026-02-27 14:19:10 -0700

    add: login visual design

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,142 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Box(
+                modifier = Modifier.fillMaxSize().background(Color.White)
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize().padding(24.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center
+
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.icon),
+                        contentDescription = "App Image",
+                        modifier = Modifier.fillMaxWidth().padding(bottom = 24.dp),
+                        contentScale = ContentScale.Fit
+                    )
+
+                    Text(
+                        text = "Hello",
+                        modifier = Modifier.padding(top = 40.dp),
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = "Welcome To Little Drop, where you manage you daily tasks",
+                        modifier = Modifier.padding(top = 16.dp),
+                        color = Color.Gray,
+                        textAlign = TextAlign.Center
+                    )
+
+                    Box(
+                        modifier = Modifier.padding(top = 32.dp)
+                        .background(
+                            color = Color(0xFF524eb6),
+                            shape = RoundedCornerShape(30.dp)
+                        )
+                        .padding(horizontal = 90.dp, vertical = 14.dp),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text(
+                            text = "Login",
+                            color = Color.White,
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 16.sp
+                        )
+                    }
+
+                    Box(
+                        modifier = Modifier.padding(top = 16.dp).fillMaxWidth()
+                        .padding(horizontal = 60.dp, vertical = 12.dp)
+                        .border(
+                            width = 2.dp,
+                            color = Color(0xFF524eb6),
+                            shape = RoundedCornerShape(30.dp)
+                        )
+                        .padding(vertical = 14.dp),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text(
+                            text = "Sign Up",
+                            color = Color(0xFF524eb6),
+                            fontSize = 16.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                    Text(
+                        text = "Sign up using",
+                        color = Color.Gray,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier.fillMaxWidth().padding(top = 24.dp)
+                    )
+                    Row(
+                        modifier = Modifier.padding(top = 16.dp),
+                        horizontalArrangement = Arrangement.Center,
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.facebook),
+                            contentDescription = "Facebook",
+                            modifier = Modifier.size(40.dp)
+                        )
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        Image(
+                            painter = painterResource(id = R.drawable.github),
+                            contentDescription = "Github",
+                            modifier = Modifier.size(40.dp)
+                        )
+
+                        Spacer(modifier = Modifier.width(20.dp))
+
+                        Image(
+                            painter = painterResource(id = R.drawable.linkedln),
+                            contentDescription = "Linkedin",
+                            modifier = Modifier.size(40.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
